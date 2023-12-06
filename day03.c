b68decfdbac34f1c778b752563624a3860c30423
b0nk <fsantos.pt@hotmail.com>
2023-12-06 22:42:16 +0000

    day03 - part 2

--- day03.c
+++ day03.c
@@ -10,6 +10,10 @@ typedef struct{
 	int value, x, y, length;
 } Part;
 
+typedef struct{
+	int value, x, y;
+} Gear;
+
 int is_digit(char c){
 	return 48 <= c && c <= 57;
 }
@@ -20,6 +24,18 @@ int is_dot(char c){
 
 int is_symbol(char c){
 	return !is_dot(c) && !is_digit(c);
+}
+
+int is_asterisk(char c){
+	return c == '*';
+}
+
+Gear create_gear(int x, int y, int v){
+	Gear g;
+	g.x = x;
+	g.y = y;
+	g.value = v;
+	return g;
 }
 
 Part create_part(int v, int x, int y){
@@ -40,7 +56,7 @@ Part create_part(int v, int x, int y){
 	return new_part;
 }
 
-int is_valid_part(char** e, Part p){
+int is_valid_part(char** e, Part p, Gear g[], int* g_counter){
 	int line = p.x;
 	int curs = p.y;
 	int end_curs = p.y + p.length;
@@ -50,15 +66,27 @@ int is_valid_part(char** e, Part p){
 		if(line - 1 >= 0){
 			if(curs - 1 >= 0){
 				if(is_symbol(e[line - 1][curs - 1])){
+					if(is_asterisk(e[line - 1][curs - 1])){
+						g[*g_counter] = create_gear(line - 1, curs - 1, p.value);
+						*g_counter += 1;
+					}
 					return 1;
 				}
 			}
 			if(curs + 1 < STACK_SIZE){
 				if(is_symbol(e[line - 1][curs + 1])){
+					if(is_asterisk(e[line - 1][curs + 1])){
+						g[*g_counter] = create_gear(line - 1, curs + 1, p.value);
+						*g_counter += 1;
+					}
 					return 1;
 				}
 			}
 			if(is_symbol(e[line - 1][curs])){
+				if(is_asterisk(e[line - 1][curs])){
+					g[*g_counter] = create_gear(line - 1, curs, p.value);
+					*g_counter += 1;
+				}
 				return 1;
 			}
 		}
@@ -66,27 +94,47 @@ int is_valid_part(char** e, Part p){
 		if(line + 1 < STACK_SIZE){
 			if(curs - 1 >= 0){
 				if(is_symbol(e[line + 1][curs - 1])){
+					if(is_asterisk(e[line + 1][curs - 1])){
+						g[*g_counter] = create_gear(line + 1, curs - 1, p.value);
+						*g_counter += 1;
+					}
 					return 1;
 				}
 			}
 			if(curs + 1 < STACK_SIZE){
 				if(is_symbol(e[line + 1][curs + 1])){
+					if(is_asterisk(e[line + 1][curs + 1])){
+						g[*g_counter] = create_gear(line + 1, curs + 1, p.value);
+						*g_counter += 1;
+					}
 					return 1;
 				}
 			}
 			if(is_symbol(e[line + 1][curs])){
+				if(is_asterisk(e[line + 1][curs])){
+					g[*g_counter] = create_gear(line + 1, curs, p.value);
+					*g_counter += 1;
+				}
 				return 1;
 			}
 		}
 		//left
 		if(curs - 1 >= 0){
 			if(is_symbol(e[line][curs - 1])){
+				if(is_asterisk(e[line][curs - 1])){
+					g[*g_counter] = create_gear(line, curs - 1, p.value);
+					*g_counter += 1;
+				}
 				return 1;
 			}
 		}
 		//right
 		if(curs + 1 < STACK_SIZE){
 			if(is_symbol(e[line][curs + 1])){
+				if(is_asterisk(e[line][curs + 1])){
+					g[*g_counter] = create_gear(line, curs + 1, p.value);
+					*g_counter += 1;
+				}
 				return 1;
 			}
 		}
@@ -110,14 +158,26 @@ void get_part_numbers(char** engine, Part* parts, int* parts_counter){
 	}
 }
 
+int find_matching_gear(Gear gears[], int idx, int counter){
+	int res = 0;
+	int base_x = gears[idx].x;
+	int base_y = gears[idx].y;
+	for(int i = idx + 1 ; i < counter; i++){
+		if(gears[i].x == base_x && gears[i].y == base_y){
+			res = gears[i].value;
+		}
+	}
+	return res;
+}
+
 int main(int argc, char* argv){
 
 	char line[BUFSIZ];
 	char** engine_map;
 	Part parts[MAX_PARTS];
-	int parts_counter = 0;
-	int line_counter = 0;
-	int sum1 = 0;
+	Gear gears[MAX_PARTS];
+	int parts_counter, gears_counter, line_counter = 0;
+	int sum1, sum2 = 0;
 
 	engine_map = malloc(sizeof(*engine_map) * STACK_SIZE);
 	for(int i = 0; i < STACK_SIZE; i++){
@@ -131,13 +191,19 @@ int main(int argc, char* argv){
 
 	get_part_numbers(engine_map, parts, &parts_counter);
 	for(int i = 0; i < parts_counter; i++){
-		if(is_valid_part(engine_map, parts[i])){
+		if(is_valid_part(engine_map, parts[i], gears, &gears_counter)){
 			sum1 += parts[i].value;
 		}
 	}
 
+	for(int i = 0; i < gears_counter; i++){
+		int v1 = gears[i].value;
+		int v2 = find_matching_gear(gears, i, gears_counter);
+		sum2 += v1 * v2;
+	}
+
 	printf("part1: %d\n", sum1);
-	printf("part2: %d\n", 0);
+	printf("part2: %d\n", sum2);
 
 	return 0;
 }
