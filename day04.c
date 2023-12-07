263bb49bd398d96c561405a55c43a51ebc98b4de
b0nk <fsantos.pt@hotmail.com>
2023-12-07 01:40:55 +0000

    day04 - part 2

--- day04.c
+++ day04.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 #define MAX_CARDS 50
 #define MAX_DECKS 250
 
 
-typedef struct{
+typedef struct Card{
 	int winning[MAX_CARDS];
 	int hand[MAX_CARDS];
 	int winning_count;
@@ -16,14 +15,15 @@ typedef struct{
 } Card;
 
 Card* create_card(){
-	Card* c = (Card*) malloc(sizeof(Card));
+	Card* c = malloc(sizeof(Card));
 	c->winning_count = 0;
 	c->hand_count = 0;
 	c->wins = 0;
 	return c;
 }
 
-void fill_card(Card* c, char* line){
+Card* fill_card(char* line){
+	Card* c = create_card();
 	char* full_game_ptr;
 	char* line_ptr;
 
@@ -52,14 +52,16 @@ void fill_card(Card* c, char* line){
 		c->hand_count += 1;
 		hand_token = strtok_r(NULL, " ", &hand_ptr);
 	}
+	return c;
 }
 
 int main(int argc, char* argv){
 
 	char line[BUFSIZ];
 	Card** cards;
+	int copies[MAX_DECKS];
 	int card_counter = 0;
-	double sum1, sum2 = 0;
+	int sum1, sum2 = 0;
 
 	cards = malloc(sizeof(*cards) * MAX_DECKS);
 	for(int i = 0; i < MAX_DECKS; i++){
@@ -68,9 +70,11 @@ int main(int argc, char* argv){
 
 	while(fgets(line, BUFSIZ, stdin) != NULL) {
 		line[strcspn(line, "\n")] = 0;
-		cards[card_counter] = create_card();
-		fill_card(cards[card_counter], line);
-		card_counter++;
+		cards[card_counter++] = fill_card(line);
+	}
+
+	for(int i = 0; i < card_counter; i++){
+		copies[i] = 1;
 	}
 
 	for(int i = 0; i < card_counter; i++){
@@ -82,12 +86,19 @@ int main(int argc, char* argv){
 			}
 		}
 		if(cards[i]->wins > 0){
-			sum1 += (int)pow(2, cards[i]->wins - 1);
+			sum1 += ( 1 << cards[i]->wins ) >> 1; //https://stackoverflow.com/questions/47416967/what-is-the-difference-between-1-x-and-pow2-x
+			for(int j = 0; j < cards[i]->wins; j++) {
+				copies[j + i + 1] += copies[i];
+			}	
 		}
 	}
 
-	printf("part1: %d\n", (int)sum1);
-	printf("part2: %d\n", (int)sum2);
+	for(int i = 0; i < card_counter; i++){
+		sum2 += copies[i];
+	}
+
+	printf("part1: %d\n", sum1);
+	printf("part2: %d\n", sum2);
 
 	return 0;
 }
