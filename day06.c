561db9d5fe389ab1623128d3d0c07c648a90d149
b0nk <fsantos.pt@hotmail.com>
2023-12-21 10:23:48 +0000

    day06 - part 2

--- day06.c
+++ day06.c
@@ -13,13 +13,14 @@ int main(int argc, char** argv){
 
 	char line[BUFSIZ];
 	int part1 = 1;
-	int part2 = 0;
+	unsigned long part2 = 0;
 
 	Race** races = malloc(sizeof(Race*) * 10);
 	int n_races = 0;
 	int parse_time = 1;
 	int** wins = malloc(sizeof(int*) * 10);
-	int n_wins = 0;
+	char *full_time_str = strdup(""), *full_distance_str = strdup("");
+	unsigned long full_time, full_distance;
 
 	while(fgets(line, BUFSIZ, stdin) != NULL) {
 		line[strcspn(line, "\n")] = 0;
@@ -31,6 +32,9 @@ int main(int argc, char** argv){
 			while(tkn != NULL){
 				int v;
 				sscanf(tkn, "%d", &v);
+				char* vchar = strdup("");
+				sprintf(vchar, "%d", v);
+				full_time_str = strcat(full_time_str, vchar);
 				races[n_races] = malloc(sizeof(Race));
 				races[n_races]->wins = 0;
 				races[n_races++]->time = v;
@@ -46,6 +50,9 @@ int main(int argc, char** argv){
 			while(tkn != NULL){
 				int v;
 				sscanf(tkn, "%d", &v);
+				char* vchar = strdup("");
+				sprintf(vchar, "%d", v);
+				full_distance_str = strcat(full_distance_str, vchar);
 				races[n_races++]->distance = v;
 				tkn = strtok(NULL, " ");
 			}
@@ -60,9 +67,18 @@ int main(int argc, char** argv){
 		}
 		part1 *= races[i]->wins;
 	}
+
+	sscanf(full_time_str, "%lu", &full_time);
+	sscanf(full_distance_str, "%lu", &full_distance);
+
+	for(int i = 0; i < full_time; i++){
+		if(i * (full_time - i) > full_distance){
+			part2++;
+		}
+	}
 	
 	printf("part1: %d\n", part1);
-	printf("part2: %d\n", part2);
+	printf("part2: %lu\n", part2);
 
 	return 0;
 }
